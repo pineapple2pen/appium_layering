07a27b688db37ef3b7a43af3b15bfd9c044f66ca
pineapple2pen <18814228173@163.com>
2018-06-15 15:19:17 +0800

    删除文件

--- page/start_page.py
+++ page/start_page.py
@@ -2,4 +2,11 @@
 # @Author : Pineapple Dong ^_^
 # @Time   : 2018/5/28 10:09
 # @File   : start_page.py
+from appium.webdriver import Remote
+
+
+class StartPage:
+    def __init__(self, driver: Remote):
+        self.driver = driver
+
 
