466f6d6f304dcfc1d68a178ab4a72d80243c656d
dongbo <dongbo@baiqishi.com>
2018-06-22 16:31:46 +0800

    执行用例初步构想

---
+++ test_runner/case_runner.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# @Author : Pineapple Dong ^_^
+# @Time   : 2018/6/22 14:55
+# @File   : case_runner.py
+
+
+class CaseRunner:
+    def get_config(self):
+        pass
+
+    def get_appium(self):
+        pass
+
+    def get_test_case(self):
+        pass
+
+    def run_case(self):
+        pass
+
+
+if __name__ == "__main__":
+    pass
